899f88530201f2dfd4fd0e4ea9a554e574ee2c94
suhao <simplesuhao@163.com>
2016-08-24 23:24:06 +0800

    lesson  two
    如何使用函数

--- lessonOne/movies.py
+++ lessonOne/movies.py
@@ -1,11 +1,27 @@
+import nester
+
 movies = ['The Holy Grail', 1975,
           'Terry Jones & Terry Gilliam', 91,
-          ['Graham Chapman', ['Michael Palin','John Cleese',
+          ['Graham Chapman', ['Michael Palin', 'John Cleese',
                               'Terry Gilliam', 'Eric Idle',
                               'Terry Jones']]]
+
+'''
 for movie in movies:
     if isinstance(movie, list):
         for mov in movie:
-            print(mov)
+            if isinstance(mov, list):
+                for mv in mov:
+                    print(mv)
+            else:
+                print(mov)
     else:
         print(movie)
+'''
+
+
+'''
+新增print_lol函数，用于自动打印列表
+'''
+
+nester.print_lol(movies)
